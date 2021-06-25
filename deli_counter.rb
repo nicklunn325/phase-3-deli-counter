f0253ea240c4b0c154d62c471b418ef422a984c4
flatlandnick <nicholas.lunn@flatironschool.com>
2021-06-25 10:59:46 -0500

    commit tree 7a2849ba21d3f14013964f01e2470d6e998dc397

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+# ["Logan", "Avi", "Spencer"]
+def line(names)
+    if names.empty?
+        puts "The line is currently empty."
+    else
+        # "The line is currently: 1. Logan 2. Avi 3. Spencer"
+        puts "The line is currently: #{names.map.with_index(1){|name, i| "#{i}. #{name}"}.join(" ")}"
+        # string = "The line is currently:"
+        # names.each.with_index(1) do |name, index|
+        #     string += " #{index}. #{name}"
+        # end
+        # puts string
+    end
+end
+
+def take_a_number(line, name)
+    line << name
+    puts "Welcome, #{name}. You are number #{line.length} in line."
+end
+
+def now_serving(line)
+    if line.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{line.shift()}."
+    end
+end
